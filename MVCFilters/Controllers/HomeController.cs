f360155f9a35ba85d9047a690383bdf5abfb53b7
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-08-11 10:39:26 +0530

    Add project files.

---
+++ MVCFilters/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFilters.Models;
+
+namespace MVCFilters.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCDBEntities dc = new MVCDBEntities();
+        public ViewResult DisplayDepts()
+        {
+            return View(dc.Departments);
+        }
+
+        [ChildActionOnly]
+        public ViewResult DisplayEmpsByDept(int Did)
+        {
+            var Emps = from E in dc.Employees where E.Did == Did select E;
+            return View(Emps);
+        }
+    }
+}
